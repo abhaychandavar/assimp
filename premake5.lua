3e3713d4a9e983f286e72c64f66b8d8ec064960b
Abhay Chandavar <abhaychandavar11@gmail.com>
2026-04-06 23:43:59 +0530

    fixed premake file

--- premake5.lua
+++ premake5.lua
@@ -6,30 +6,27 @@ project "Assimp"
     targetdir ("bin/" .. outputDir)
     objdir    ("bin-int/" .. outputDir)
 
-    -- Root and generated headers
     includedirs
     {
         ".",
         "include",
         "code",
-        "contrib/zlib",
-        "contrib/pugixml/src",
+        "contrib/zlib",         -- zconf.h / zlib.h (FBX binary compression)
+        "contrib/unzip",        -- ZipArchiveIOSystem.cpp (always compiled in Common)
+        "contrib/pugixml/src",  -- FBX XML format
         "contrib/utf8cpp/source",
         "contrib/poly2tri",
-        "contrib/openddlparser/include",
-        "contrib/unzip",
-        "contrib/zip/src",
+        "contrib",              -- resolves <stb/stb_image.h> → contrib/stb/stb_image.h
         "contrib/rapidjson/include",
-        "contrib/stb",
     }
 
-    -- Core assimp sources
     files
     {
         "include/assimp/**.h",
         "include/assimp/**.hpp",
         "include/assimp/**.inl",
 
+        -- Core (always required)
         "code/Common/**.cpp",
         "code/Common/**.h",
         "code/CApi/**.cpp",
@@ -41,62 +38,11 @@ project "Assimp"
         "code/Material/**.cpp",
         "code/Material/**.h",
 
-        -- Asset importers/exporters (all formats)
-        "code/AssetLib/3DS/**.cpp",   "code/AssetLib/3DS/**.h",
-        "code/AssetLib/3MF/**.cpp",   "code/AssetLib/3MF/**.h",
-        "code/AssetLib/AC/**.cpp",    "code/AssetLib/AC/**.h",
-        "code/AssetLib/AMF/**.cpp",   "code/AssetLib/AMF/**.h",
-        "code/AssetLib/ASE/**.cpp",   "code/AssetLib/ASE/**.h",
-        "code/AssetLib/Assbin/**.cpp","code/AssetLib/Assbin/**.h",
-        "code/AssetLib/Assjson/**.cpp","code/AssetLib/Assjson/**.h",
-        "code/AssetLib/Assxml/**.cpp","code/AssetLib/Assxml/**.h",
-        "code/AssetLib/B3D/**.cpp",   "code/AssetLib/B3D/**.h",
-        "code/AssetLib/BVH/**.cpp",   "code/AssetLib/BVH/**.h",
-        "code/AssetLib/Blender/**.cpp","code/AssetLib/Blender/**.h",
-        "code/AssetLib/COB/**.cpp",   "code/AssetLib/COB/**.h",
-        "code/AssetLib/CSM/**.cpp",   "code/AssetLib/CSM/**.h",
-        "code/AssetLib/Collada/**.cpp","code/AssetLib/Collada/**.h",
-        "code/AssetLib/DXF/**.cpp",   "code/AssetLib/DXF/**.h",
-        "code/AssetLib/FBX/**.cpp",   "code/AssetLib/FBX/**.h",
-        "code/AssetLib/HMP/**.cpp",   "code/AssetLib/HMP/**.h",
-        "code/AssetLib/IFC/**.cpp",   "code/AssetLib/IFC/**.h",
-        "code/AssetLib/IQM/**.cpp",   "code/AssetLib/IQM/**.h",
-        "code/AssetLib/Irr/**.cpp",   "code/AssetLib/Irr/**.h",
-        "code/AssetLib/LWO/**.cpp",   "code/AssetLib/LWO/**.h",
-        "code/AssetLib/LWS/**.cpp",   "code/AssetLib/LWS/**.h",
-        "code/AssetLib/MD2/**.cpp",   "code/AssetLib/MD2/**.h",
-        "code/AssetLib/MD3/**.cpp",   "code/AssetLib/MD3/**.h",
-        "code/AssetLib/MD5/**.cpp",   "code/AssetLib/MD5/**.h",
-        "code/AssetLib/MDC/**.cpp",   "code/AssetLib/MDC/**.h",
-        "code/AssetLib/MDL/**.cpp",   "code/AssetLib/MDL/**.h",
-        "code/AssetLib/MMD/**.cpp",   "code/AssetLib/MMD/**.h",
-        "code/AssetLib/MS3D/**.cpp",  "code/AssetLib/MS3D/**.h",
-        "code/AssetLib/NDO/**.cpp",   "code/AssetLib/NDO/**.h",
-        "code/AssetLib/NFF/**.cpp",   "code/AssetLib/NFF/**.h",
-        "code/AssetLib/OFF/**.cpp",   "code/AssetLib/OFF/**.h",
-        "code/AssetLib/Obj/**.cpp",   "code/AssetLib/Obj/**.h",
-        "code/AssetLib/Ogre/**.cpp",  "code/AssetLib/Ogre/**.h",
-        "code/AssetLib/OpenGEX/**.cpp","code/AssetLib/OpenGEX/**.h",
-        "code/AssetLib/Ply/**.cpp",   "code/AssetLib/Ply/**.h",
-        "code/AssetLib/Q3BSP/**.cpp", "code/AssetLib/Q3BSP/**.h",
-        "code/AssetLib/Q3D/**.cpp",   "code/AssetLib/Q3D/**.h",
-        "code/AssetLib/Raw/**.cpp",   "code/AssetLib/Raw/**.h",
-        "code/AssetLib/SIB/**.cpp",   "code/AssetLib/SIB/**.h",
-        "code/AssetLib/SMD/**.cpp",   "code/AssetLib/SMD/**.h",
-        "code/AssetLib/STEPParser/**.cpp","code/AssetLib/STEPParser/**.h",
-        "code/AssetLib/STL/**.cpp",   "code/AssetLib/STL/**.h",
-        "code/AssetLib/Step/**.cpp",  "code/AssetLib/Step/**.h",
-        "code/AssetLib/Terragen/**.cpp","code/AssetLib/Terragen/**.h",
-        "code/AssetLib/Unreal/**.cpp","code/AssetLib/Unreal/**.h",
-        "code/AssetLib/X/**.cpp",     "code/AssetLib/X/**.h",
-        "code/AssetLib/X3D/**.cpp",   "code/AssetLib/X3D/**.h",
-        "code/AssetLib/XGL/**.cpp",   "code/AssetLib/XGL/**.h",
-        "code/AssetLib/glTF/**.cpp",  "code/AssetLib/glTF/**.h",
-        "code/AssetLib/glTF2/**.cpp", "code/AssetLib/glTF2/**.h",
-        "code/AssetLib/glTFCommon/**.cpp","code/AssetLib/glTFCommon/**.h",
-        "code/Pbrt/**.cpp",           "code/Pbrt/**.h",
+        -- Importers: FBX + OBJ only
+        "code/AssetLib/FBX/**.cpp", "code/AssetLib/FBX/**.h",
+        "code/AssetLib/Obj/**.cpp", "code/AssetLib/Obj/**.h",
 
-        -- Contrib: zlib (bundled)
+        -- zlib (FBX binary uses deflate)
         "contrib/zlib/adler32.c",
         "contrib/zlib/compress.c",
         "contrib/zlib/crc32.c",
@@ -113,53 +59,87 @@ project "Assimp"
         "contrib/zlib/uncompr.c",
         "contrib/zlib/zutil.c",
 
-        -- Contrib: pugixml
+        -- unzip (ZipArchiveIOSystem lives in Common, always compiled)
+        "contrib/unzip/ioapi.c",
+        "contrib/unzip/unzip.c",
+
+        -- pugixml (FBX XML)
         "contrib/pugixml/src/pugixml.cpp",
 
-        -- Contrib: poly2tri
+        -- poly2tri (tessellation in PostProcessing)
         "contrib/poly2tri/poly2tri/common/shapes.cc",
         "contrib/poly2tri/poly2tri/sweep/advancing_front.cc",
         "contrib/poly2tri/poly2tri/sweep/cdt.cc",
         "contrib/poly2tri/poly2tri/sweep/sweep.cc",
         "contrib/poly2tri/poly2tri/sweep/sweep_context.cc",
-
-        -- Contrib: openddlparser (needed by OpenGEX importer)
-        "contrib/openddlparser/code/DDLNode.cpp",
-        "contrib/openddlparser/code/OpenDDLCommon.cpp",
-        "contrib/openddlparser/code/OpenDDLExport.cpp",
-        "contrib/openddlparser/code/OpenDDLParser.cpp",
-        "contrib/openddlparser/code/OpenDDLStream.cpp",
-        "contrib/openddlparser/code/Value.cpp",
-
-        -- Contrib: unzip (needed by IFC importer)
-        "contrib/unzip/ioapi.c",
-        "contrib/unzip/unzip.c",
-
-        -- Contrib: zip/miniz (needed by 3MF exporter)
-        "contrib/zip/src/zip.c",
     }
 
     defines
     {
-        -- Disable formats that need extra SDKs or are off by default
-        "ASSIMP_BUILD_NO_C4D_IMPORTER",
-        "ASSIMP_BUILD_NO_M3D_IMPORTER",
-        "ASSIMP_BUILD_NO_M3D_EXPORTER",
+        -- Disable all exporters (removes glTF/glTF2 rapidjson issues entirely)
+        "ASSIMP_BUILD_NO_EXPORT",
+
+        -- Disable every importer except FBX and OBJ
         "ASSIMP_BUILD_NO_USD_IMPORTER",
-        "ASSIMP_BUILD_NO_VRML_IMPORTER",
-        -- openddlparser
-        "OPENDDLPARSER_BUILD",
-        -- zlib: disable unaligned loads for safety
+        "ASSIMP_BUILD_NO_X_IMPORTER",
+        "ASSIMP_BUILD_NO_AMF_IMPORTER",
+        "ASSIMP_BUILD_NO_3DS_IMPORTER",
+        "ASSIMP_BUILD_NO_MD3_IMPORTER",
+        "ASSIMP_BUILD_NO_MDL_IMPORTER",
+        "ASSIMP_BUILD_NO_MD2_IMPORTER",
+        "ASSIMP_BUILD_NO_PLY_IMPORTER",
+        "ASSIMP_BUILD_NO_ASE_IMPORTER",
+        "ASSIMP_BUILD_NO_HMP_IMPORTER",
+        "ASSIMP_BUILD_NO_SMD_IMPORTER",
+        "ASSIMP_BUILD_NO_MDC_IMPORTER",
+        "ASSIMP_BUILD_NO_MD5_IMPORTER",
+        "ASSIMP_BUILD_NO_STL_IMPORTER",
+        "ASSIMP_BUILD_NO_LWO_IMPORTER",
+        "ASSIMP_BUILD_NO_DXF_IMPORTER",
+        "ASSIMP_BUILD_NO_NFF_IMPORTER",
+        "ASSIMP_BUILD_NO_RAW_IMPORTER",
+        "ASSIMP_BUILD_NO_SIB_IMPORTER",
+        "ASSIMP_BUILD_NO_OFF_IMPORTER",
+        "ASSIMP_BUILD_NO_AC_IMPORTER",
+        "ASSIMP_BUILD_NO_BVH_IMPORTER",
+        "ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+        "ASSIMP_BUILD_NO_IRR_IMPORTER",
+        "ASSIMP_BUILD_NO_Q3D_IMPORTER",
+        "ASSIMP_BUILD_NO_B3D_IMPORTER",
+        "ASSIMP_BUILD_NO_COLLADA_IMPORTER",
+        "ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+        "ASSIMP_BUILD_NO_CSM_IMPORTER",
+        "ASSIMP_BUILD_NO_3D_IMPORTER",
+        "ASSIMP_BUILD_NO_LWS_IMPORTER",
+        "ASSIMP_BUILD_NO_OGRE_IMPORTER",
+        "ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+        "ASSIMP_BUILD_NO_MS3D_IMPORTER",
+        "ASSIMP_BUILD_NO_COB_IMPORTER",
+        "ASSIMP_BUILD_NO_BLEND_IMPORTER",
+        "ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+        "ASSIMP_BUILD_NO_NDO_IMPORTER",
+        "ASSIMP_BUILD_NO_IFC_IMPORTER",
+        "ASSIMP_BUILD_NO_XGL_IMPORTER",
+        "ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+        "ASSIMP_BUILD_NO_GLTF_IMPORTER",
+        "ASSIMP_BUILD_NO_GLTF1_IMPORTER",
+        "ASSIMP_BUILD_NO_GLTF2_IMPORTER",
+        "ASSIMP_BUILD_NO_C4D_IMPORTER",
+        "ASSIMP_BUILD_NO_3MF_IMPORTER",
+        "ASSIMP_BUILD_NO_X3D_IMPORTER",
+        "ASSIMP_BUILD_NO_MMD_IMPORTER",
+        "ASSIMP_BUILD_NO_M3D_IMPORTER",
+        "ASSIMP_BUILD_NO_IQM_IMPORTER",
+        "ASSIMP_BUILD_NO_PBRT_IMPORTER",
+
+        -- Misc
         "MINIZ_USE_UNALIGNED_LOADS_AND_STORES=0",
     }
 
-    -- IFC reader gen files are very large; need /bigobj on MSVC
-    filter "files:code/AssetLib/IFC/IFCReaderGen*"
-        buildoptions { "/bigobj" }
-    filter {}
-
+    -- IFC files are removed, but keep /bigobj for large FBX reader files just in case
     filter "system:windows"
         defines { "WIN32_LEAN_AND_MEAN", "_CRT_SECURE_NO_WARNINGS", "_SCL_SECURE_NO_WARNINGS" }
+        buildoptions { "/bigobj" }
 
     filter "configurations:Debug"
         runtime "Debug"
